23a15f526650eed19efb197979660a1dfb929b06
suraj9716 <ss971600@gmail.com>
2022-06-17 13:03:12 +0530

    pushing girls,kids,megamenu,vaccations,women,sandal pages

---
+++ Suraj all work/girlsinner/girlsinner.js
@@ -0,0 +1,216 @@
+var kidsdata = [{image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/aa/P00650800_d2.jpg",
+image1:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/45/P00677840_b1.jpg",
+                  name:"MARC JACOBS KIDS",
+                  desc:"Leopard-print rucksack",
+                  price:79,
+                },
+                {image:"https://img.mytheresa.com/1088/1088/66/jpeg/catalog/product/fc/P00650805.jpg",
+                  name:"MARC JACOBS KIDS",
+                  desc:"Printed cotton dress",
+                  price:271,
+                },
+                {image:"https://img.mytheresa.com/1088/1088/66/jpeg/catalog/product/70/P00650807.jpg",
+                  name:"MARC JACOBS KIDS",
+                  desc:"Cotton dress",
+                  price:45,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/bd/P00610485_d2.jpg",
+                  name:"NEW BALANCE KIDS",
+                  desc:"574 Core suede sneakers",
+                  price:45,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/58/P00659568_d2.jpg",
+                  name:"THE ANIMALS OBSERVATORY",
+                  desc:"Straw hat",
+                  price:42,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/e9/P00650528_d2.jpg",
+                  name:"CHLOÉ KIDS",
+                  desc:"Logo backpack",
+                  price:143,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/10/P00652023_d2.jpg",
+                  name:"KENZO KIDS",
+                  desc:"Rubber sandals",
+                  price:38,
+                },
+                {image:"https://img.mytheresa.com/1088/1088/66/jpeg/catalog/product/ab/P00651984.jpg",
+                  name:"KENZO KIDS",
+                  desc:"Printed cotton dress",
+                  price:114,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/4a/P00652245_d2.jpg",
+                  name:"KENZO KIDS",
+                  desc:"Logo sandals",
+                  price:94,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/8a/P00682565_b1.jpg",
+                  name:"MONNALISA",
+                  desc:"Baby floral neoprene skirt",
+                  price:105,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/01/P00595490_d2.jpg",
+                  name:"CHLOÉ KIDS",
+                  desc:"Metallic leather ballet flats",
+                  price:134,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/48/P00650866_d2.jpg",
+                  name:"MARC JACOBS KIDS",
+                  desc:"Leopard-print thong sandals",
+                  price:325,
+                },
+                {image:"https://img.mytheresa.com/1088/1088/66/jpeg/catalog/product/ec/P00650816.jpg",
+                  name:"MARC JACOBS KIDS",
+                  desc:"Pouch-appliqué chambray dress",
+                  price:115,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/d1/P00682576_b1.jpg",
+                  name:"MONNALISA",
+                  desc:"Baby tulle-trimmed cotton onesie",
+                  price:115,
+                },
+                
+]
+var kidsarr = JSON.parse(localStorage.getItem("cartdetail"))|| []
+document.querySelector("#filter").addEventListener("change",priceFilter)
+priceFilter(kidsdata)
+var count = kidsdata.length
+console.log(count)
+
+
+function priceFilter(){
+     var selected = document.querySelector("#filter").value;
+     if(selected=="lowtohigh"){
+      kidsdata.sort(function(a,b){
+        if(a.price>b.price) return 1
+        if(a.price<b.price) return -1
+        return 0
+        // console.log(a.price)
+       })
+      //  disptable(menarr)
+      // console.log(selected)
+     } 
+     if(selected=="sortby"){
+      kidsdata.sort(function(a,b){
+        return 
+        // console.log(a.price)
+       })
+      //  disptable(menarr)
+      // console.log(selected)
+     } 
+     if(selected=="hightolow"){
+      kidsdata.sort(function(a,b){
+        if(a.price>b.price) return -1
+        if(a.price<b.price) return 1
+        return 0
+       })
+      //  disptable(menarr)
+     }
+     disptable(kidsdata) 
+    }
+  function brandfilter(){
+
+  }
+disptable(kidsdata)
+function disptable(k){
+    document.getElementById("productlength").innerText = `${count} products `
+    document.querySelector("#right_Section").innerHTML="";
+   k.forEach(function(el,index){
+    
+    var div = document.createElement("div")
+    var avtar = document.createElement("img")
+    // var avtar1 = document.createElement("img")
+    avtar.setAttribute("src",el.image)
+    // avtar.addEventListener("mouseover",function(){
+    //   changeimage(kidsdata,index)
+    //   // console.log(el.image)
+    // })
+    var p1 = document.createElement("p")
+    p1.innerText = el.name
+    var p2 = document.createElement("p")
+    p2.innerText = el.desc
+    var h4 = document.createElement("h4")
+    h4.innerText = `€ ${el.price}`
+    var btn = document.createElement("button")
+    btn.innerText = "Add to Cart"
+    btn.addEventListener("click",function(){
+      addall(el)
+   })
+    div.append(avtar,p1,p2,h4,btn)
+    document.getElementById("right_Section").append(div)
+   })
+  //  function changeimage(kidsdata,index){
+  //   console.log(kidsdata.image1)
+   
+  //  }
+}
+function addall(el){
+  //  el.quantity = 1;
+  //  el.total_price = el.price * 1; 
+
+   kidsarr.push(el)
+   localStorage.setItem("cartdetail",JSON.stringify(kidsarr))
+   
+   
+ }
+ let ArrayData=[];
+let allData=document.getElementsByClassName("check");
+Array.from(allData).forEach(function(e,i){
+  document.getElementsByClassName("check")[i].addEventListener("click",function(){
+    let checkbox=document.getElementsByClassName("check")[i]
+  
+    // console.log(checkbox.value);
+    if(checkbox.checked==true){
+
+
+      ArrayData.push(checkbox.value)
+      CheckboxFun(ArrayData);
+  
+    }
+    else{
+      let index=ArrayData.indexOf(checkbox.value);
+      ArrayData.splice(index,1);
+      CheckboxFun(ArrayData);
+    
+    }
+  })
+})
+function CheckboxFun(arr){
+  let newArray=[];
+
+  if(arr.length==0){
+    disptable(kidsdata)
+    return;
+  }
+  arr.forEach(function(e){
+    let result=kidsdata.filter(function(x){
+      return x.name==e;
+    })
+    // console.log(result)
+   newArray.push(result)
+  
+  })
+  // console.log(newArray);
+  let lastArray=[];
+  for(let i=0;i<newArray.length;i++){
+        for(let j=0;j<newArray[i].length;j++){
+          // console.log(newArray[i][j])
+          lastArray.push(newArray[i][j])
+        }
+  }
+  disptable(lastArray);
+}
+// shwocheckbox()
+// var show = false;
+// function shwocheckbox(){
+//   var checkbo = document.getElementById("hide");
+//   if(!show){
+//     checkbo.style.display = "block";
+//     show=true;
+//   } else{
+//     checkbo.style.display = "none";
+//     show=false;
+//   }
+// }
+
+disptable(kidsdata)
---
+++ Suraj all work/kidsinner/kidsinner.js
@@ -0,0 +1,216 @@
+var kidsdata = [{image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/e6/P00577458_d2.jpg",
+image1:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/45/P00677840_b1.jpg",
+                  name:"BURBERRY KIDS",
+                  desc:"Baby Vintage Check cotton-blend bucket hat",
+                  price:170,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/45/P00677840_b1.jpg",
+                  name:"GUCCI KIDS",
+                  desc:"Baby Web stripe pleated denim dress",
+                  price:380,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/bf/P00686010_b1.jpg",
+                  name:"MINI RODINI",
+                  desc:"Baby embroidered cotton romper",
+                  price:68,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/ff/P00655997_b1.jpg",
+                  name:"BAPE KIDS",
+                  desc:"Striped socks",
+                  price:35,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/cd/P00677977_b1.jpg",
+                  name:"GUCCI KIDS",
+                  desc:"Baby Square G wool cardigan",
+                  price:545,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/55/P00543157_d2.jpg",
+                  name:"MOSCHINO KIDS",
+                  desc:"Baby leather sneakers",
+                  price:130,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/72/P00677984_b1.jpg",
+                  name:"GUCCI KIDS",
+                  desc:"Baby GG intarsia wool pants",
+                  price:295,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/10/P00677983_b1.jpg",
+                  name:"GUCCI KIDS",
+                  desc:"Baby GG intarsia sweater",
+                  price:400,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/37/P00682610_b1.jpg",
+                  name:"MONNALISA",
+                  desc:"Baby floral down coat",
+                  price:210,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/8a/P00682565_b1.jpg",
+                  name:"MONNALISA",
+                  desc:"Baby floral neoprene skirt",
+                  price:105,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/e4/P00711451_d2.jpg",
+                  name:"MONCLER ENFANT",
+                  desc:"Baby logo wool beanie",
+                  price:95,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/3b/P00708472_b1.jpg",
+                  name:"MONCLER ENFANT",
+                  desc:"Baby Macaire quilted coat",
+                  price:325,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/0f/P00682559_b1.jpg",
+                  name:"MONNALISA",
+                  desc:"Baby cotton top and pants set",
+                  price:115,
+                },
+                {image:"https://img.mytheresa.com/560/560/33/jpeg/catalog/product/d1/P00682576_b1.jpg",
+                  name:"MONNALISA",
+                  desc:"Baby tulle-trimmed cotton onesie",
+                  price:115,
+                },
+                
+]
+var kidsarr = JSON.parse(localStorage.getItem("cartdetail"))|| []
+document.querySelector("#filter").addEventListener("change",priceFilter)
+priceFilter(kidsdata)
+var count = kidsdata.length
+console.log(count)
+
+
+function priceFilter(){
+     var selected = document.querySelector("#filter").value;
+     if(selected=="lowtohigh"){
+      kidsdata.sort(function(a,b){
+        if(a.price>b.price) return 1
+        if(a.price<b.price) return -1
+        return 0
+        // console.log(a.price)
+       })
+      //  disptable(menarr)
+      // console.log(selected)
+     } 
+     if(selected=="sortby"){
+      kidsdata.sort(function(a,b){
+        return 
+        // console.log(a.price)
+       })
+      //  disptable(menarr)
+      // console.log(selected)
+     } 
+     if(selected=="hightolow"){
+      kidsdata.sort(function(a,b){
+        if(a.price>b.price) return -1
+        if(a.price<b.price) return 1
+        return 0
+       })
+      //  disptable(menarr)
+     }
+     disptable(kidsdata) 
+    }
+  function brandfilter(){
+
+  }
+disptable(kidsdata)
+function disptable(k){
+    document.getElementById("productlength").innerText = `${count} products `
+    document.querySelector("#right_Section").innerHTML="";
+   k.forEach(function(el,index){
+    
+    var div = document.createElement("div")
+    var avtar = document.createElement("img")
+    // var avtar1 = document.createElement("img")
+    avtar.setAttribute("src",el.image)
+    // avtar.addEventListener("mouseover",function(){
+    //   changeimage(kidsdata,index)
+    //   // console.log(el.image)
+    // })
+    var p1 = document.createElement("p")
+    p1.innerText = el.name
+    var p2 = document.createElement("p")
+    p2.innerText = el.desc
+    var h4 = document.createElement("h4")
+    h4.innerText = `€ ${el.price}`
+    var btn = document.createElement("button")
+    btn.innerText = "Add to Cart"
+    btn.addEventListener("click",function(){
+      addall(el)
+   })
+    div.append(avtar,p1,p2,h4,btn)
+    document.getElementById("right_Section").append(div)
+   })
+  //  function changeimage(kidsdata,index){
+  //   console.log(kidsdata.image1)
+   
+  //  }
+}
+function addall(el){
+  //  el.quantity = 1;
+  //  el.total_price = el.price * 1; 
+
+   kidsarr.push(el)
+   localStorage.setItem("cartdetail",JSON.stringify(kidsarr))
+   
+   
+ }
+ let ArrayData=[];
+let allData=document.getElementsByClassName("check");
+Array.from(allData).forEach(function(e,i){
+  document.getElementsByClassName("check")[i].addEventListener("click",function(){
+    let checkbox=document.getElementsByClassName("check")[i]
+  
+    // console.log(checkbox.value);
+    if(checkbox.checked==true){
+
+
+      ArrayData.push(checkbox.value)
+      CheckboxFun(ArrayData);
+  
+    }
+    else{
+      let index=ArrayData.indexOf(checkbox.value);
+      ArrayData.splice(index,1);
+      CheckboxFun(ArrayData);
+    
+    }
+  })
+})
+function CheckboxFun(arr){
+  let newArray=[];
+
+  if(arr.length==0){
+    disptable(kidsdata)
+    return;
+  }
+  arr.forEach(function(e){
+    let result=kidsdata.filter(function(x){
+      return x.name==e;
+    })
+    // console.log(result)
+   newArray.push(result)
+  
+  })
+  // console.log(newArray);
+  let lastArray=[];
+  for(let i=0;i<newArray.length;i++){
+        for(let j=0;j<newArray[i].length;j++){
+          // console.log(newArray[i][j])
+          lastArray.push(newArray[i][j])
+        }
+  }
+  disptable(lastArray);
+}
+// shwocheckbox()
+// var show = false;
+// function shwocheckbox(){
+//   var checkbo = document.getElementById("hide");
+//   if(!show){
+//     checkbo.style.display = "block";
+//     show=true;
+//   } else{
+//     checkbo.style.display = "none";
+//     show=false;
+//   }
+// }
+
+disptable(kidsdata)
