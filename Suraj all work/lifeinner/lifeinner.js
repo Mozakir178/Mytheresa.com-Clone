4a0bc187e361e81951b357f6a5f8c31c890b1b7f
suraj9716 <ss971600@gmail.com>
2022-06-17 19:12:18 +0530

    again added life page

---
+++ Suraj all work/lifeinner/lifeinner.js
@@ -0,0 +1,362 @@
+var kidsdata = [{
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/d0/P00679222_d2.jpg",
+    desc: "Abd El Kader Classic candle",
+    price: 85,
+    strikedoffprice: "€ 100",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/d0/P00679343_d2.jpg",
+    desc: "Cire Classic candle",
+    price: 90,
+    strikedoffprice: "€ 119",
+    name: "CIRE TRUDON",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/44/P00678396_d2.jpg",
+    desc: "x Versace Alphabet set of 7",
+    price: 270,
+    strikedoffprice: "€ 554",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/86/P00678090_d2.jpg",
+    desc: "Nera Small bowl",
+    price: 110,
+    strikedoffprice: "€ 212",
+    name: "ZANAT",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/26/P00679376_d2.jpg",
+    desc: "Abd El Kader alabaster candle",
+    price: 160,
+    strikedoffprice: "€ 242",
+    name: "CIRE TRUDON",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/af/P00679280_d2.jpg",
+    desc: "St. Tropez Soleil book",
+    price: 95,
+    strikedoffprice: "€ 150",
+    name: "ASSOULINE",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/e7/P00679311_d2.jpg",
+    desc: "x Comme des Garçons Comme des Fornà Large Otto candle",
+    price: 290,
+    strikedoffprice: "€ 444",
+    name: "FORNASETTI PROFUMI",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/6f/P00679217_d2.jpg",
+    desc: "Bo Maki Small vase",
+    price: 265,
+    strikedoffprice: "€ 555",
+    name: "CASSINA",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/cc/P00678143_d2.jpg",
+    desc: "Lampe De Bureau table lamp",
+    price: 265,
+    strikedoffprice: "€ 444",
+    name: "VITRA",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/2b/P00678300_d2.jpg",
+    desc: "Jean-Michel Basquiat book",
+    price: 150,
+    strikedoffprice: "€ 322",
+    name: "TASCHEN",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/5c/P00678133_d2.jpg",
+    desc: "Chronopak desk clock",
+    price: 490,
+    strikedoffprice: "€ 987",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/95/P00678272_d2.jpg",
+    desc: "Swirl Medium vase",
+    price: 565,
+    strikedoffprice: "€ 876",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/1b/P00678842_d2.jpg",
+    desc: "Botanical Large tablecloth",
+    price: 315,
+    strikedoffprice: "€ 666",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/9c/P00678563_d2.jpg",
+    desc: "Idra set of 6 water glasses",
+    price: 240,
+    strikedoffprice: "€ 444",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/1a/P00678093_d2.jpg",
+    desc: "Nera Large bowl",
+    price: 175,
+    strikedoffprice: "€ 300",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/51/P00678392_d2.jpg",
+    desc: "x Versace Baroque Bianco",
+    price: 70,
+    strikedoffprice: "€ 100",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/2e/P00679310_d2.jpg",
+    desc: "Capri Dolce Vita book",
+    price: 95,
+    strikedoffprice: "€ 231",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/51/P00678791_d2.jpg",
+    desc: "Iron tray",
+    price: 115,
+    strikedoffprice: "€ 170",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/cc/P00679374_d2.jpg",
+    desc: "Ernesto alabaster candle",
+    price: 160,
+    strikedoffprice: "€ 240",
+    name: "CIRE TRUDON"
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/d6/P00678858_d2.jpg",
+    desc: "Libellula set of 2 dinner plates",
+    price: 110,
+    strikedoffprice: "€ 180",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/83/P00679349_d2.jpg",
+    desc: "Margot set of 2 red wine",
+    price: 220,
+    strikedoffprice: "€ 340",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/28/P00678179_d2.jpg",
+    desc: "Panton Junior chair",
+    price: 200,
+    strikedoffprice: "€ 300",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/dc/P00678063_d2.jpg",
+    desc: "Unity stool",
+    price: 2,
+    strikedoffprice: "€ 3",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/1c/P00678714_d2.jpg",
+    desc: "Lazy Susan tray",
+    price: 600,
+    strikedoffprice: "€ 900",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/7c/P00703800_d2.jpg",
+    desc: "Small printed silk twill cushio",
+    price: 395,
+    strikedoffprice: "€ 456",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/4b/P00703770_d2.jpg",
+    desc: "Porcelain charger plate",
+    price: 225,
+    strikedoffprice: "€ 300",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/9f/P00678599_d2.jpg",
+    desc: "Humbert wool-blend cushio",
+    price: 135,
+    strikedoffprice: "€ 200",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/14/P00678075_d2.jpg",
+    desc: "Nera stool",
+    price: 670,
+    strikedoffprice: "€ 890",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/21/P00703798_d2.jpg",
+    desc: "Medium printed canvas cushion",
+    price: 395,
+    strikedoffprice: "€ 549",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/13/P00703752_d2.jpg",
+    desc: "\de_en\dolce-gabbana-casa-murano-red-wine-glass-2256803.html",
+    price: 350,
+    strikedoffprice: "€ 600",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/48/P00703766_d2.jpg",
+    desc: "Set of 2 porcelain soup plates",
+    price: 265,
+    strikedoffprice: "€ 500",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/12/P00678168_d2.jpg",
+    desc: "Chaise Tout Bois chair",
+    price: 770,
+    strikedoffprice: "€ 1111",
+    name: "ROSENTHAL",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/04/P00678885_d2.jpg",
+    desc: "Aphrodite cushion",
+    price: 350,
+    strikedoffprice: "€ 543",
+    name: "ZANAT",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/48/P00678609_d2.jpg",
+    desc: "Totem High candle",
+    price: 370,
+    strikedoffprice: "€ 555",
+    name: "ZANAT",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/ee/P00679226_d2.jpg",
+    desc: "Il Viaggio di Nettuno set of 4 bread plates",
+    price: 185,
+    strikedoffprice: "€ 222",
+    name: "ZANAT",
+}, {
+    image: "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/c3/P00678447_d2.jpg",
+    desc: "Ascot set of 2 crystal glasses",
+    price: 585,
+    strikedoffprice: "€ 700",
+    name: "ZANAT",
+},
+
+]
+var kidsarr = JSON.parse(localStorage.getItem("cartdetail"))|| []
+document.querySelector("#filter").addEventListener("change",priceFilter)
+priceFilter(kidsdata)
+var count = kidsdata.length
+console.log(count)
+
+
+function priceFilter(){
+var selected = document.querySelector("#filter").value;
+if(selected=="lowtohigh"){
+kidsdata.sort(function(a,b){
+if(a.price>b.price) return 1
+if(a.price<b.price) return -1
+return 0
+// console.log(a.price)
+})
+//  disptable(menarr)
+// console.log(selected)
+} 
+if(selected=="sortby"){
+kidsdata.sort(function(a,b){
+return 
+// console.log(a.price)
+})
+//  disptable(menarr)
+// console.log(selected)
+} 
+if(selected=="hightolow"){
+kidsdata.sort(function(a,b){
+if(a.price>b.price) return -1
+if(a.price<b.price) return 1
+return 0
+})
+//  disptable(menarr)
+}
+disptable(kidsdata) 
+}
+function brandfilter(){
+
+}
+disptable(kidsdata)
+function disptable(k){
+document.getElementById("productlength").innerText = `${count} products `
+document.querySelector("#right_Section").innerHTML="";
+k.forEach(function(el,index){
+
+var div = document.createElement("div")
+var avtar = document.createElement("img")
+// var avtar1 = document.createElement("img")
+avtar.setAttribute("src",el.image)
+// avtar.addEventListener("mouseover",function(){
+//   changeimage(kidsdata,index)
+//   // console.log(el.image)
+// })
+var p1 = document.createElement("p")
+p1.innerText = el.name
+var p2 = document.createElement("p")
+p2.innerText = el.desc
+var h4 = document.createElement("h4")
+h4.innerText = `€ ${el.price}`
+var btn = document.createElement("button")
+btn.innerText = "Add to Cart"
+btn.addEventListener("click",function(){
+addall(el)
+})
+div.append(avtar,p1,p2,h4,btn)
+document.getElementById("right_Section").append(div)
+})
+//  function changeimage(kidsdata,index){
+//   console.log(kidsdata.image1)
+
+//  }
+}
+function addall(el){
+//  el.quantity = 1;
+//  el.total_price = el.price * 1; 
+
+kidsarr.push(el)
+localStorage.setItem("cartdetail",JSON.stringify(kidsarr))
+
+
+}
+let ArrayData=[];
+let allData=document.getElementsByClassName("check");
+Array.from(allData).forEach(function(e,i){
+document.getElementsByClassName("check")[i].addEventListener("click",function(){
+let checkbox=document.getElementsByClassName("check")[i]
+
+// console.log(checkbox.value);
+if(checkbox.checked==true){
+
+
+ArrayData.push(checkbox.value)
+CheckboxFun(ArrayData);
+
+}
+else{
+let index=ArrayData.indexOf(checkbox.value);
+ArrayData.splice(index,1);
+CheckboxFun(ArrayData);
+
+}
+})
+})
+function CheckboxFun(arr){
+let newArray=[];
+
+if(arr.length==0){
+disptable(kidsdata)
+return;
+}
+arr.forEach(function(e){
+let result=kidsdata.filter(function(x){
+return x.name==e;
+})
+// console.log(result)
+newArray.push(result)
+
+})
+// console.log(newArray);
+let lastArray=[];
+for(let i=0;i<newArray.length;i++){
+for(let j=0;j<newArray[i].length;j++){
+  // console.log(newArray[i][j])
+  lastArray.push(newArray[i][j])
+}
+}
+disptable(lastArray);
+}
+// shwocheckbox()
+// var show = false;
+// function shwocheckbox(){
+//   var checkbo = document.getElementById("hide");
+//   if(!show){
+//     checkbo.style.display = "block";
+//     show=true;
+//   } else{
+//     checkbo.style.display = "none";
+//     show=false;
+//   }
+// }
+
+disptable(kidsdata)
